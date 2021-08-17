ac6c366a8b748263f9368f80d8d60a0781e8b39f
Hope Kim <kzang2028@gmail.com>
2021-08-17 18:27:38 +0900

    Initial commit

---
+++ my_settings.py
@@ -0,0 +1,16 @@
+
+#from dogifier.settings import DATABASES
+
+
+SECRET_KEY = 'django-insecure-vngil%wl%d!#7vzmyx=huji6i5jg%^wcq!w=tf^o7b&#vgv&=b'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dog_profile',
+        'USER': 'root',
+        'PASSWORD': 'wkdgh7235', 
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
